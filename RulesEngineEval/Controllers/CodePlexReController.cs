e62e436a573abee38e2032b888dda03995de06ec
John Ryan <jnyryan@gmail.com>
2012-08-31 17:07:25 +0100

    initial commit

---
+++ RulesEngineEval/Controllers/CodePlexReController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RulesEngine;
+using RulesEngineEval.Models;
+
+namespace RulesEngineEval.Controllers
+{
+    public class CodePlexReController : Controller
+    {
+        //
+        // GET: /CodePlexRe/
+
+        public ActionResult Simple()
+        {
+            Person person = new Person();
+            person.Name = "Bill";
+            person.Phone = "1234214";
+            person.DateOfBirth = new DateTime(2000, 10, 2);
+            RunSimple(person);
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult Simple(Person person)
+        {
+            RunSimple(person);
+            return View(person);
+        }
+        
+        public static void RunSimple(Person person)
+        {
+            Engine engine = new Engine();
+            engine.For<Person>()
+                    .Setup(p => p.DateOfBirth)
+                        .MustBeLessThan(DateTime.Now)
+                    .Setup(p => p.Name)
+                        .MustNotBeNull()
+                        .MustMatchRegex("^[a-zA-z]+$")
+                    .Setup(p => p.Phone)
+                        .MustNotBeNull()
+                        .MustMatchRegex("^[0-9]+$");
+            
+            person.IsValid = engine.Validate(person).ToString();
+
+        }
+
+
+        public ActionResult CrossField()
+        {
+            var user = new NewUserRegistration() { UserName = "user1", Password = "123", ConfirmPassword = "123" };
+            RunCrossField(user);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult CrossField(NewUserRegistration user)
+        {
+            RunCrossField(user);
+            return View(user);
+        }
+
+        public static void RunCrossField(NewUserRegistration user)
+        {
+            Engine engine = new Engine();
+            engine.For<NewUserRegistration>()
+                .Setup(u => user.UserName)
+                    .MustNotBeNullOrEmpty()
+                .Setup(u => user.Password)
+                    .MustNotBeNullOrEmpty()
+                .Setup(u => user.ConfirmPassword)
+                    .MustEqual(u => user.Password);
+
+            user.IsValid = engine.Validate(user).ToString();
+
+        }
+
+        public ActionResult ObjectGraphValidation()
+        {
+            var chessClub = new Club("Chess Club"
+                 , new Member("athoma13", "athoma13@codeplex.com")
+                 , new Member("Arpad Elo", "arpad@hotmail.com")
+                 , new Member("Garry Kasparov", "garry@gmail.com")
+                 , new Member("Bobby B", "bobby@aol.com")
+                 );
+
+            RunObjectGraphValidation(chessClub);
+            return View(chessClub);
+        }
+
+        [HttpPost]
+        public ActionResult ObjectGraphValidation(Club club)
+        {
+            club = new Club("Chess Club"
+                 , new Member("athoma13", "athoma13@codeplex.com")
+                 , new Member("Arpad Elo", "arpad@hotmail.com")
+                 , new Member("Garry Kasparov", "garry@gmail.com")
+                 , new Member("", "bobby@aol.com")
+                 );
+            RunObjectGraphValidation(club);
+            return View(club);
+        }
+
+        public static void RunObjectGraphValidation(Club club)
+        {
+            Engine engine = new Engine();
+
+            engine.For<Member>()
+                .Setup(member => member.Name)
+                    .MustNotBeNullOrEmpty()
+                .Setup(member => member.Email)
+                    .MustNotBeNullOrEmpty();
+
+            engine.For<Club>()
+                .Setup(c => club.President)
+                    .MustNotBeNull()
+                    .CallValidate() //Calls engine.Validate for the President.
+                .Setup(c => club.Members)
+                    .MustNotBeNull()
+                    .CallValidateForEachElement(); //Calls engine.Validate() on each element
+
+            club.IsValid = engine.Validate(club).ToString();
+
+        }
+
+
+        public ActionResult CustomRules()
+        {
+            var member = new Member { Name = "athoma13", Email = "athoma13@codeplex.com" };
+            RunCustomRules(member);
+            return View(member);
+        }
+
+        [HttpPost]
+        public ActionResult CustomRules(Member member)
+        {
+            RunCustomRules(member);
+            return View(member);
+        }
+
+        public static void RunCustomRules(Member member)
+        {
+            Engine engine = new Engine();
+
+            engine.For<Member>()
+               .Setup(m => member.Name)
+                   .MustNotBeNullOrEmpty()
+               .Setup(m => member.Email)
+                   .MustNotBeNullOrEmpty()
+                   .MustPassRule(new EmailRule());
+
+            
+            member.IsValid = engine.Validate(member).ToString();
+
+        }
+
+        public ActionResult ErrorMessages()
+        {
+            var employee = new Employee() { Email = "bill@hotmail.com", EmployeeNumber = 123, Name = "bill" }; 
+            RunErrorMessages(employee);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult ErrorMessages(Employee employee)
+        {
+            RunErrorMessages(employee);
+            return View(employee);
+        }
+
+        public static void RunErrorMessages(Employee employee)
+        {
+            var engine = new Engine();
+            engine.For<Person>()
+                    .Setup(m => m.Name)
+                        .MustNotBeNullOrEmpty()
+                        .WithMessage("Name must not be empty") //Only when name is null or empty
+                        .MustMatchRegex(@"^[a-z]+$")
+                        .WithMessage("Name contains invalid characters") //Only when name contains invalid characters
+
+                    .Setup(m => m.Email)
+                        .WithMessage("Invalid email address") //For any rule breakers on the Email property.
+                        .MustNotBeNullOrEmpty()
+                        .MustMatchRegex(@"^[a-z]+@[a-z]+(\.[a-z]+)*$");
+
+            engine.For<Employee>() //Member will inherit rules set for a Person.
+                    .Setup(m => m.EmployeeNumber)
+                        .WithMessage("Employee number is not valid")
+                        .MustBeGreaterThan(0);
+
+            
+            var report = new ValidationReport(engine);
+            var result = report.Validate(employee).ToString();
+            var errors = report.GetErrorMessages(employee);
+            employee.IsValid = result;
+            string flatErrors = string.Empty;
+            foreach (string s in errors)
+                flatErrors += s;
+            employee.Errors = flatErrors;
+
+            
+
+
+        }
+
+
+    }
+}
---
+++ RulesEngineEval/Models/AccountModels.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using RE = RulesEngine;
+
+namespace RulesEngineEval.Models
+{
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String IsValid { get; set; }
+        public String Errors { get; set; }
+    }
+
+    public class NewUserRegistration
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public String IsValid { get; set; }
+    }
+
+    public class Member
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public String IsValid { get; set; }
+
+        public Member() { }
+        public Member(string name, string email)
+        {
+            this.Name = name;
+            this.Email = email;
+        }
+        
+    }
+
+    public class Club
+    {
+        private readonly List<Member> _members = new List<Member>();
+        public string Name { get; set; }
+        public Member President { get; set; }
+        public String IsValid { get; set; }
+
+        public List<Member> Members
+        {
+            get { return _members; }
+        }
+
+        public Club() { }
+
+        public Club(string name, Member president, params Member[] members)
+        {
+            this.Name = name;
+            this.President = president;
+            this._members.AddRange(members);
+        }
+    }
+
+    public class EmailRule : RE.IRule<string>
+    {
+        public RE.ValidationResult Validate(string value)
+        {
+            if (value == null
+                || System.Text.RegularExpressions.Regex.IsMatch(value
+                    , @"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$"
+                    , System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+            {
+                return RE.ValidationResult.Success;
+            }
+            else
+            {
+                return RE.ValidationResult.Fail(value);
+            }
+        }
+
+        public string RuleKind
+        {
+            get { return this.GetType().Name; }
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+
+
+
+    public class ChangePasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LogOnModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
